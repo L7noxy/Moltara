b3d9ab78d7579027b3f62e4ae09b2d73d4b19976
GabrielCastanhel <gabrielcastanhel2@gmail.com>
2025-08-03 20:27:41 -0300

    Colocando pacotes no backend

--- 4-fase-Moltara/src/App.jsx
+++ 4-fase-Moltara/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,7 +6,7 @@ function App() {
   return (
     <>
       <div>
-
+        
       </div>
 
     </>
