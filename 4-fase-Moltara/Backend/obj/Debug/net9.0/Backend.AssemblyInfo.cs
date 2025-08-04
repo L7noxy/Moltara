2aea7965472b2f9183ba3cacb6b8bdc0cc0c9952
GabrielCastanhel <gabrielcastanhel2@gmail.com>
2025-08-03 22:14:51 -0300

    Arrumando a porta do backend

--- 4-fase-Moltara/Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ 4-fase-Moltara/Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3d9ab78d7579027b3f62e4ae09b2d73d4b19976")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85c4561779e69ff61d1ec20baecb83e632a16153")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
