dbbd7093575453f3016835fd4a97d155b425aa58
GabrielCastanhel <gabrielcastanhel2@gmail.com>
2025-08-18 20:46:22 -0300

    Colocando o EstoqueProduto.css

--- 4-fase-Moltara/Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ 4-fase-Moltara/Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50dc17f31ec7f9d1b1901b8c8731fafbbd261548")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37327f222a9425ffc5fd5601987c2949e55d8f72")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
