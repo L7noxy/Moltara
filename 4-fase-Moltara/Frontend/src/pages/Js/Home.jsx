4aa4840ab4a3dc0775384a084317dd38a5eada83
L7noxy <gabrielcastanhel2@gmail.com>
2025-09-03 15:40:53 -0300

    Melhorando a tela da home para deixar os banners como link e para deixar eles mais ajustados a mudanças de tela

--- 4-fase-Moltara/Frontend/src/pages/Js/Home.jsx
+++ 4-fase-Moltara/Frontend/src/pages/Js/Home.jsx
@@ -24,9 +24,10 @@ export default function Home() {
           <CardProduto />
 
           <div className='banner-produto-home'>
-            <img src="./img/Banner_900x100.png" alt="" srcset="" />
-            <img src="./img/Zowie.jpg" alt="" />
+            <a href="https://www.pichau.com.br/marca/benq"><img src="./img/Zowie.jpg" alt="" /></a>
+            <a href="https://www.pichau.com.br/regrasintel">  <img src="./img/Banner_900x100.png" alt="" srcset="" /></a>
           </div>
+          
         </div>
       </div>
     </>
