b8ac218a378a5160ad78b738bd1c6aa77bac32db
ANA CAROLINA ANDRADE DE ARAUJO <ana_ca_araujo@estudante.sesisenai.org.br>
2025-10-28 17:19:01 -0300

    Finalizando peqeunos detalhes visuais

--- 4-fase-Moltara/Frontend/src/pages/Js/ProdutoDetalhada.jsx
+++ 4-fase-Moltara/Frontend/src/pages/Js/ProdutoDetalhada.jsx
@@ -1,44 +1,44 @@
 import React from 'react'
 import Navbar from '../../components/Js/Navbar'
+import Footer from '../../components/Js/Footer'
 import '../Css/ProdutoDetalhada.css'
-
 import { useState } from 'react'
 
 export default function ProdutoDetalhada() {
   // Estado para a quantidade do produto
-  const [quantidade, SetQuantidade]= useState(1)
-  const aumentar = () => SetQuantidade(quantidade +1)
-  const diminuir = () =>{
-    if (quantidade >1) SetQuantidade(quantidade -1)
+  const [quantidade, SetQuantidade] = useState(1)
+  const aumentar = () => SetQuantidade(quantidade + 1)
+  const diminuir = () => {
+    if (quantidade > 1) SetQuantidade(quantidade - 1)
   }
 
   // Estado para as opções de personalização selecionadas
   const [personalizacaoSelecionada, setPersonalizacaoSelecionada] = useState({
-   cor: null,
-   tamanho: null,
-   simbolo: null
+    cor: null,
+    tamanho: null,
+    simbolo: null
   });
 
   const icones = {
     Estrela: {
-     contorno: '/imgs/estrela.png',       
-     preenchido: '/imgs/estrela_preenchida.png'    
+      contorno: '/img/estrela.png',
+      preenchido: '/img/estrela_preenchida.png'
     },
     Casinha: {
-      contorno: '/imgs/casinha.png',       
-      preenchido: '/imgs/casinha_preenchida.png'     
+      contorno: '/img/casinha.png',
+      preenchido: '/img/casinha_preenchida.png'
     },
     Circulo: {
-      contorno: '/imgs/circulo.png',  
-      preenchido: '/imgs/circulo_preenchido.png'   
+      contorno: '/img/circulo.png',
+      preenchido: '/img/circulo_preenchido.png'
     }
   };
 
   // Função para lidar com a mudança de personalização
   const handlePersonalizacaoChange = (categoria, valor) => {
     setPersonalizacaoSelecionada(prev => ({
-     ...prev,
-     [categoria]: valor
+      ...prev,
+      [categoria]: valor
     }));
   };
 
@@ -50,11 +50,11 @@ export default function ProdutoDetalhada() {
           <div className='container-item'>
             <h1>IMG produto</h1>
           </div>
-    
+
           <div className='alinhamento-imgs'>
-            <div className='container-img'/>
-            <div className='container-img'/>
-            <div className='container-img'/>
+            <div className='container-img' />
+            <div className='container-img' />
+            <div className='container-img' />
           </div>
         </div>
 
@@ -70,92 +70,89 @@ export default function ProdutoDetalhada() {
               <div className="grupo-personalizacao">
                 <p className='text-personalizacao'>Cores (Selecionada: {personalizacaoSelecionada.cor || 'Nenhuma'})</p>
                 <div className='checkbox-container'>
-                  <button 
+                  <button
                     className={`cor-button cor-vermelho ${personalizacaoSelecionada.cor === "Vermelho" ? 'selected' : ''}`}
                     onClick={() => handlePersonalizacaoChange('cor', 'Vermelho')}
                     aria-label="Selecionar cor Vermelha"
                   />
 
-                  <button 
+                  <button
                     className={`cor-button cor-azul ${personalizacaoSelecionada.cor === "Azul" ? 'selected' : ''}`}
                     onClick={() => handlePersonalizacaoChange('cor', 'Azul')}
                     aria-label="Selecionar cor Azul"
                   />
 
-                  <button 
+                  <button
                     className={`cor-button cor-amarelo ${personalizacaoSelecionada.cor === "Amarelo" ? 'selected' : ''}`}
                     onClick={() => handlePersonalizacaoChange('cor', 'Amarelo')}
                     aria-label="Selecionar cor Amarela"
                   />
-                  
+
                 </div>
               </div>
 
               <div className="grupo-personalizacao">
                 <p className='text-personalizacao'>Tamanhos (Selecionado: {personalizacaoSelecionada.tamanho || 'Nenhum'})</p>
-                
-                <div className='checkbox-container'>
-                <div className='checkbox-container'>
-                  <button 
-                    className={`tamanho-button tamanho-pequeno ${personalizacaoSelecionada.tamanho === "Pequeno" ? 'selected' : ''}`}
-                    onClick={() => handlePersonalizacaoChange('tamanho', 'Pequeno')}
-                  >
-                   P
-                  </button>
 
-                  <button 
-                    className={`tamanho-button tamanho-medio ${personalizacaoSelecionada.tamanho === "Médio" ? 'selected' : ''}`}
-                    onClick={() => handlePersonalizacaoChange('tamanho', 'Médio')}
-                  >
-                   M
-                  </button>
+                <div className='checkbox-container'>
+                  <div className='checkbox-container'>
+                    <button
+                      className={`tamanho-button tamanho-pequeno ${personalizacaoSelecionada.tamanho === "Pequeno" ? 'selected' : ''}`}
+                      onClick={() => handlePersonalizacaoChange('tamanho', 'Pequeno')}
+                    >
+                      P
+                    </button>
 
-                  <button 
-                    className={`tamanho-button tamanho-grande ${personalizacaoSelecionada.tamanho === "Grande" ? 'selected' : ''}`}
-                    onClick={() => handlePersonalizacaoChange('tamanho', 'Grande')}
-                  >
-                   G
-                  </button>
-                 </div>
+                    <button
+                      className={`tamanho-button tamanho-medio ${personalizacaoSelecionada.tamanho === "Médio" ? 'selected' : ''}`}
+                      onClick={() => handlePersonalizacaoChange('tamanho', 'Médio')}
+                    >
+                      M
+                    </button>
+
+                    <button
+                      className={`tamanho-button tamanho-grande ${personalizacaoSelecionada.tamanho === "Grande" ? 'selected' : ''}`}
+                      onClick={() => handlePersonalizacaoChange('tamanho', 'Grande')}
+                    >
+                      G
+                    </button>
+                  </div>
                 </div>
 
                 <div className="grupo-personalizacao">
                   <p className='text-personalizacao'>Símbolos (Selecionado: {personalizacaoSelecionada.simbolo || 'Nenhum'})</p>
                   <div className='checkbox-container'>
-                    
-                    {/* Botão Estrela */}
-                    <button 
+
+                    <button
                       className={`simbolo-button ${personalizacaoSelecionada.simbolo === "Estrela" ? 'selected' : ''}`}
                       onClick={() => handlePersonalizacaoChange('simbolo', 'Estrela')}
                       aria-label="Selecionar símbolo Estrela"
                     >
-                      <img 
-                        src={personalizacaoSelecionada.simbolo === "Estrela" ? icones.Estrela.filled : icones.Estrela.outline} 
-                        alt="Estrela" 
+                      <img
+                        src={personalizacaoSelecionada.simbolo === "Estrela" ? icones.Estrela.preenchido : icones.Estrela.contorno}
+                        alt="Estrela"
                       />
                     </button>
 
-                    {/* Botão Casa */}
-                    <button 
+                    <button
                       className={`simbolo-button ${personalizacaoSelecionada.simbolo === "Casa" ? 'selected' : ''}`}
                       onClick={() => handlePersonalizacaoChange('simbolo', 'Casa')}
                       aria-label="Selecionar símbolo Casa"
                     >
-                      <img 
-                        src={personalizacaoSelecionada.simbolo === "Casa" ? icones.Casa.filled : icones.Casa.outline} 
-                        alt="Casa" 
+                      <img
+                        src={personalizacaoSelecionada.simbolo === "Casa" ? icones.Casinha.preenchido: icones.Casinha.contorno}
+                        alt="Casa"
                       />
                     </button>
-                    
-                    {/* Botão Círculo */}
-                    <button 
+
+                    <button
                       className={`simbolo-button ${personalizacaoSelecionada.simbolo === "Circulo" ? 'selected' : ''}`}
                       onClick={() => handlePersonalizacaoChange('simbolo', 'Circulo')}
                       aria-label="Selecionar símbolo Círculo"
                     >
-                      <img 
-                        src={personalizacaoSelecionada.simbolo === "Circulo" ? icones.Circulo.filled : icones.Circulo.outline} 
-                        alt="Círculo" 
+                      <img
+                        src={personalizacaoSelecionada.simbolo === "Circulo" ? icones.Circulo.preenchido : icones.Circulo.contorno}
+                        alt="Círculo"
                       />
                     </button>
                   </div>
@@ -165,25 +162,26 @@ export default function ProdutoDetalhada() {
             </div>
           </div>
         </div>
-      
+
         <div className='container-valor'>
           <h3 className='titulo-valor'>Preço total: xxx</h3>
           <div className='alinhamento-quantidade-produto'>
             <label>Quantidade do produto:</label>
 
-            <div className='grupo-botoes-input'> 
-              <button className='diminuir' onClick={diminuir}>-</button> 
+            <div className='grupo-botoes-input'>
+              <button className='diminuir' onClick={diminuir}>-</button>
               <input type='number' value={quantidade} readOnly></input>
               <button className='aumentar' onClick={aumentar}>+</button>
             </div>
           </div>
-          
+
           <div className='botoes-compra'>
             <button className='button-adicionar'>Adicionar ao Carrinnho</button>
             <button className='button-confirmar'>Confirmar Compra</button>
           </div>
         </div>
       </div>
+      <Footer />
     </div>
   )
 }
