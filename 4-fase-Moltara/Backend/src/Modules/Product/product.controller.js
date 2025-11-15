2f076b5bc464ee65865891ae867819e54e1184da
GabrielCastanhel <gabrielcastanhel2@gmail.com>
2025-11-15 19:51:53 -0300

    Terminando de colocar o endpoint para as imagem

--- 4-fase-Moltara/Backend/src/Modules/Product/product.controller.js
+++ 4-fase-Moltara/Backend/src/Modules/Product/product.controller.js
@@ -1,50 +1,67 @@
-import Produto from '../Product/product.schema.js';
+import Produto from "../Product/product.schema.js";
+import cloudinary from "../../Config/cloudinary.config.js";
 
 export const cadastrarProduto = async (req, res) => {
-  const { nome, descricao, preco, estoque, id } = req.body;
+    const { nome, descricao, preco, estoque } = req.body;
+    
+    const file = req.file;
 
-  try {
-    const novoProduto = new Produto({
-      nome,
-      preco,
-      descricao,
+    let imageUrl = null;
+    let imagePublicId = null;
 
-    });
+    try {
+        if (file) {
+            const result = await cloudinary.uploader.upload(file.buffer.toString('base64'), {
+                folder: "produtos-api", // Pasta de destino no Cloudinary
+                resource_type: "auto"
+            });
 
-    await novoProduto.save();
+            // 3. Salva os dados retornados do Cloudinary
+            imageUrl = result.secure_url;
+            imagePublicId = result.public_id;
+        }
 
-    return res.status(201).json({
-      nome: novoProduto.nome,
-      preco: novoProduto.preco,
-      descricao: novoProduto.descricao,
-    });
+        const novoProduto = new Produto({
+            nome,
+            preco,
+            descricao,
+            estoque,
+            imagemUrl: imageUrl,
+            imagemPublicId: imagePublicId,
+        });
 
-    console.log("Produto cadastrado com sucesso:", novoProduto);
+        await novoProduto.save();
 
-  } catch (error) {
-    console.error("Erro no cadastro do produto", error);
+        return res.status(201).json({
+            nome: novoProduto.nome,
+            preco: novoProduto.preco,
+            descricao: novoProduto.descricao,
+            imagemUrl: novoProduto.imagemUrl,
+        });
 
-    if (error.name === "ValidationError") {
-      return res.status(400).json({ message: error.message });
+    } catch (error) {
+        console.error("Erro no cadastro do produto:", error);
+
+        if (error.name === "ValidationError") {
+            return res.status(400).json({ message: error.message });
+        }
+
+        return res.status(500).json({
+            message: "Erro interno do servidor ao tentar finalizar o cadastro.",
+        });
     }
-
-    return res.status(500).json({
-      message: "Erro interno do servidor ao tentar finalizar o cadastro.",
-    });
-  }
 };
 
 export const getProduto = async (req, res) => {
-  try {
-    const produtos = await Produto.find({});
-    return res.status(200).json(produtos);
-  } catch (error) {
-    console.error("Erro ao buscar produtos:", error);
-    return res.status(500).json({
-      message: "Erro interno do servidor ao buscar o produto.",
-    });
-  }
+    try {
+        const produtos = await Produto.find({});
+        return res.status(200).json(produtos);
+    } catch (error) {
+        console.error("Erro ao buscar produtos:", error);
+        return res.status(500).json({
+            message: "Erro interno do servidor ao buscar o produto.",
+        });
+    }
 };
-
 
 export default cadastrarProduto;
--- 4-fase-Moltara/Backend/src/routes/product.routes.js
+++ 4-fase-Moltara/Backend/src/routes/product.routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 const router = express.Router();
 import { cadastrarProduto, getProduto } from '../Modules/Product/product.controller.js';
+import upload from '../middlewares/multer.middleware.js';
 
-router.post('/criar', cadastrarProduto);
+router.post('/criar', upload.single('imageFile'), cadastrarProduto);
 router.get('/buscar', getProduto);
 
 export default router;
